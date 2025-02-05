ea1f8e4fa8eb4a649f18437f2efaed7a90c7db49
Shakil Ahmed <sa3akash@gmail.com>
2025-02-05 17:14:09 +0600

    fix ts error and shadcd ui install

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,18 +1,7 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  presets: [require('../../libs/ui/tailwind.config')],
+  content: ['./src/**/*.{ts,tsx}', '../../libs/ui/**/*.{ts,tsx}'],
 } satisfies Config;
